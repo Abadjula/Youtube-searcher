b74e464a05baf5060a4266bfc6827b333aba17d7
Abadjula <76713140+Abadjula@users.noreply.github.com>
2021-03-01 19:02:29 -0600

    Update main.py

--- main.py
+++ main.py
@@ -15,5 +15,5 @@ while True:
     html = urllib.request.urlopen("https://www.youtube.com/results?search_query=" + Search)
     video_id = re.findall(r"watch\?v=(\S{11})", html.read().decode())
 # Sends u the video
-    print("https://www.youtube.com/watch?v=" + video_id[0]
+    print("https://www.youtube.com/watch?v=" + video_id[0])
 # Made by Abadjula#4856
