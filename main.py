76a5cffe71b36721da379e5cedfb3d161226be15
Abadjula <76713140+Abadjula@users.noreply.github.com>
2021-03-01 18:57:15 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
-import urllib.request
-import re
-Context = input("Enter a youtube search: ")
-Search = ""
-for space in Context:
-    if space in " ":
-        Search += "+"
-    else:
-        Search += space
-html = urllib.request.urlopen("https://www.youtube.com/results?search_query=" + Search)
-video_id = re.findall(r"watch\?v=(\S{11})", html.read().decode())
-print("https://www.youtube.com/watch?v=" + video_id[0])
+while True:
+    import urllib.request
+    import re
+    Context = input("Enter a youtube search: ")
+    Search = ""
+    for space in Context:
+        if space in " ":
+            Search += "+"
+        else:
+            Search += space
+    html = urllib.request.urlopen("https://www.youtube.com/results?search_query=" + Search)
+    video_id = re.findall(r"watch\?v=(\S{11})", html.read().decode())
+    print("https://www.youtube.com/watch?v=" + video_id[0])
