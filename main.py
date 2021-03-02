a74646c1886424e1d705a73ba7f1c19efef8d845
Abadjula <76713140+Abadjula@users.noreply.github.com>
2021-03-01 19:01:24 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
+# Loops forever
 while True:
     import urllib.request
     import re
+# Asks you to enter what u want to search
     Context = input("Enter a youtube search: ")
     Search = ""
     for space in Context:
+# Making it thatevery space u make it adds a plus sign in between
         if space in " ":
             Search += "+"
         else:
             Search += space
+# decodes what u typed
     html = urllib.request.urlopen("https://www.youtube.com/results?search_query=" + Search)
     video_id = re.findall(r"watch\?v=(\S{11})", html.read().decode())
-    print("https://www.youtube.com/watch?v=" + video_id[0])
+# Sends u the video
+    print("https://www.youtube.com/watch?v=" + video_id[0]
+# Made by Abadjula#4856
